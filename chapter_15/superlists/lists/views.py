b63af0734fb7af80718f4bc0ae5c9aa40f9506d5
Razvan Blaga <[omitted]>
2017-07-27 16:03:42 +0300

    updated tests, fixed integrity errors

---
+++ chapter_15/superlists/lists/views.py
@@ -0,0 +1,35 @@
+
+from django.shortcuts import render, redirect
+from .models import List
+from .forms import ItemForm, ExistingListItemForm
+
+
+# Create your views here.
+def home_page(request):
+    return render(request, 'home.html', {'form': ItemForm()})
+
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=list_)
+
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect(list_)
+
+    return render(request, 'list.html', {'list': list_, 'form': form})
+
+
+def new_list(request):
+    form = ItemForm(data=request.POST)
+
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+
+    return render(request, 'home.html', {'form': form})
+
