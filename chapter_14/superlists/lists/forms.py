b1f1977060cb0e1e2cc2c5f7be470baaa36120d1
Razvan Blaga <[omitted]>
2017-07-27 12:06:44 +0300

    added forms and tests

---
+++ chapter_14/superlists/lists/forms.py
@@ -0,0 +1,37 @@
+
+from django import forms
+from .models import Item
+
+
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
+
+class ItemForm(forms.models.ModelForm):
+    '''
+    item_text = forms.CharField(
+        widget=forms.fields.TextInput(
+            attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg'
+            }
+        )
+    )
+    '''
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg'
+            }),
+        }
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
+        }
